2dc32c92bafe8d469285cc9368b9af978edec65e
adhfmz7 <ahmadaldasouqi559@gmail.com>
2024-01-31 11:16:37 -0800

    switch ray tracer to classes

--- lin-alg/quaternion.cc
+++ lin-alg/quaternion.cc
@@ -7,7 +7,10 @@ Quaternion::Quaternion()
 
 Quaternion::Quaternion(double w, double x, double y, double z)
 {
-
+	this->scalers[0] = w;
+	this->scalers[1] = x;
+	this->scalers[2] = y;
+	this->scalers[3] = z;
 }
 
 Quaternion Quaternion::operator-() const 
@@ -25,13 +28,14 @@ Quaternion Quaternion::normalize() const
 
 double Quaternion::magnitude() const
 {
-
+	
 }
 
 Quaternion Quaternion::inverse  () const
 {
 
 }
+
 Quaternion Quaternion::conjucate() const
 {
 
@@ -40,27 +44,39 @@ Quaternion Quaternion::conjucate() const
 Quaternion Quaternion::operator+(const Quaternion& other) const
 {
 	Quaternion new_quat;
-	
+	for (int i = 0; i < 4; ++i)	new_quat.scalers[i] = this->scalers[i] + other.scalers[i];
 
 	return new_quat;
 }
+
 Quaternion Quaternion::operator-(const Quaternion& other) const
 {
-
+	Quaternion new_quat;
+	for (int i = 0; i < 4; ++i)	new_quat.scalers[i] = this->scalers[i] - other.scalers[i];
+	return new_quat;
 }
+
 Quaternion Quaternion::operator*(const Quaternion& other) const
 {
 
 }
+
 Quaternion Quaternion::operator/(const Quaternion& other) const
 {
+	
 
 }
+
 Quaternion Quaternion::operator*(const double other) const
 {
-
+	Quaternion new_quat;
+	for (int i = 0; i < 4; ++i)	new_quat.scalers[i] = this->scalers[i] * other; 
+	return new_quat;
+	
 }
 Quaternion Quaternion::operator/(const double other) const
 {
-
+	Quaternion new_quat;
+	for (int i = 0; i < 4; ++i)	new_quat.scalers[i] = this->scalers[i] / other; 
+	return new_quat;
 }
